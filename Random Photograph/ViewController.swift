93f9c0382b0034401b372d5592d2cdfc070843f1
Lazaros Psarokostas <lazarospsa@hotmail.com>
2022-09-28 23:21:39 +0300

    Initial Commit

---
+++ Random Photograph/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photograph
+//
+//  Created by Lazaros Psarokostas on 28/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
